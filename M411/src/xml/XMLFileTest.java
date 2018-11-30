09f11008a12f4a3f7c9e0386cad9c044566e9e3b
capa <capa@cascianos-mbp-2.lan>
2018-11-30 05:18:04 +0100

    Second Commit

--- M411/src/xml/XMLFileTest.java
+++ M411/src/xml/XMLFileTest.java
@@ -1,5 +1,6 @@
 package xml;
 
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class XMLFileTest {
@@ -9,6 +10,10 @@ public class XMLFileTest {
 		//TODO create the xml file on the file system
 		XMLFile xmlFile = new XMLFile("/users/capa/music.xml");
 		NodeList cdElements = xmlFile.getElementsByTagName("CD");
+		Node n;
+		for (int i=0; i<cdElements.getLength(); i++) {
+			System.out.println(cdElements.item(i));
+		}
 	}
 
 }
