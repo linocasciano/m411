26d2f43a5f0f2dca3fbbf17b35d30e5211b5aea7
capa <capa@cascianos-mbp-2.lan>
2018-12-01 13:43:35 +0100

    XML

--- M411/src/xml/XMLFile.java
+++ M411/src/xml/XMLFile.java
@@ -2,12 +2,14 @@ package xml;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -25,7 +27,7 @@ public class XMLFile {
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			d = db.parse(f);
 			//TODO comment and explain
-			d.getDocumentElement().normalize();
+			//d.getDocumentElement().normalize();
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
@@ -42,6 +44,40 @@ public class XMLFile {
 	public Node findSubNode(String name, Node node ) {
 		return node;
 		//TODO implement
+	}
+	
+	public ArrayList<CD> getCDList() {
+		ArrayList<CD> cdList = new ArrayList<CD>();
+		NodeList nodeList = d.getDocumentElement().getChildNodes();
+		Node node = null;
+		CD cd = null;
+		for (int i=0; i<nodeList.getLength(); i++) {
+			node = nodeList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) node;
+
+                // Get the value of the ID attribute.
+                String ID = node.getAttributes().getNamedItem("ID").getNodeValue();
+
+                // Get the value of all sub-elements.
+                String firstname = elem.getElementsByTagName("Firstname")
+                                    .item(0).getChildNodes().item(0).getNodeValue();
+
+                String lastname = elem.getElementsByTagName("Lastname").item(0)
+                                    .getChildNodes().item(0).getNodeValue();
+
+                Integer age = Integer.parseInt(elem.getElementsByTagName("Age")
+                                    .item(0).getChildNodes().item(0).getNodeValue());
+
+                Double salary = Double.parseDouble(elem.getElementsByTagName("Salary")
+                                    .item(0).getChildNodes().item(0).getNodeValue());
+
+           }
+		}
+		
+		return cdList;
+		
+		
 	}
 
 }
--- M411/src/xml/XMLFileTest.java
+++ M411/src/xml/XMLFileTest.java
@@ -1,5 +1,7 @@
 package xml;
 
+import java.util.ArrayList;
+
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -8,12 +10,16 @@ public class XMLFileTest {
 	public static void main(String[] args) {
 
 		//TODO create the xml file on the file system
-		XMLFile xmlFile = new XMLFile("/users/capa/music.xml");
-		NodeList cdElements = xmlFile.getElementsByTagName("CD");
+		XMLFile xmlFile = new XMLFile("/users/capa/Employees.xml");
+		
+		
+		ArrayList<CD> cdList = xmlFile.getCDList();
+		
+		/*NodeList cdElements = xmlFile.getElementsByTagName("CD");
 		Node n;
 		for (int i=0; i<cdElements.getLength(); i++) {
 			System.out.println(cdElements.item(i));
-		}
+		}*/
 	}
 
 }
