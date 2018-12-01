a01185cc824fbdb06c29e3f88eb81329322bb88e
capa <capa@cascianos-mbp-2.lan>
2018-12-01 15:33:28 +0100

    JSON

--- M411/src/xml/WebServiceClientTest.java
+++ M411/src/xml/WebServiceClientTest.java
@@ -2,9 +2,6 @@ package xml;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class WebServiceClientTest {
@@ -13,8 +10,15 @@ public class WebServiceClientTest {
 		// TODO Auto-generated method stub
 		String host = "https://earthquake.usgs.gov/";
 	    String path = "fdsnws/event/1/";
-		String query = "query?format=xml&starttime=2016-09-29&endtime=2016-09-30&minmagnitude=5";
+	    String format = "geojson";
+		String query = "query?format=" + format + "&starttime=2016-09-29&endtime=2016-09-30&minmagnitude=5";
 		URL url = new URL(host + path + query);
+		print(url);
+		
+		
+	}
+	
+	public static void print(URL url) throws IOException {
 		WebServiceClient wsc = new WebServiceClient(url);
 		BufferedReader in = wsc.getBufferedReader();
 		String line;
