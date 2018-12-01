2ae65284fc014cb8668e8327a2252b4fe415c7fe
capa <capa@cascianos-mbp-2.lan>
2018-12-01 13:44:01 +0100

    CD

---
+++ M411/src/xml/CD.java
@@ -0,0 +1,51 @@
+package xml;
+
+public class CD {
+	
+	String title = null;
+	String country = null;
+	String artist = null;
+	Integer year = null;
+	
+	
+	
+	public CD(String title, String country, String artist, Integer year) {
+		super();
+		this.title = title;
+		this.country = country;
+		this.artist = artist;
+		this.year = year;
+	}
+	
+	public CD() {
+		// explain why
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+
+}
