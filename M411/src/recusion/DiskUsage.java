7b80398c8767753c8f1ec5e0b1069903c52efd4e
Lino <Lino@Blackbox>
2018-12-10 05:19:44 +0100

    Disk Usage

--- M411/src/recusion/DiskUsage.java
+++ M411/src/recusion/DiskUsage.java
@@ -1,10 +1,28 @@
 package recusion;
 
+import java.io.File;
+
 public class DiskUsage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long size = diskUsage(new File("C:\\Temp\\A"));
+		System.out.println("Total size is [" + size + "] Bytes.");
 
+	}
+
+	public static long diskUsage(File f) {
+		long diskUsage = 0;
+		if (!f.isDirectory()) { diskUsage = f.length(); }
+		System.out.println("Checking " + f);
+		if (f.isDirectory()) {
+			File[] fileList = f.listFiles();
+			if (fileList != null) {
+				for (File g : fileList) {
+					diskUsage += diskUsage(g);
+				}
+			}
+		}
+		return diskUsage;
 	}
 
 }
