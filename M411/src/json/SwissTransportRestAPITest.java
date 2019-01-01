9ac25a4c7c336500b72e764f5389283c31b18827
capa <capa@cascianos-mbp-2.lan>
2019-01-01 05:24:24 +0100

    Working example with JSON API

--- M411/src/json/SwissTransportRestAPITest.java
+++ M411/src/json/SwissTransportRestAPITest.java
@@ -1,5 +1,31 @@
 package json;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import helper.WebServiceClient;
+
 public class SwissTransportRestAPITest {
+	
+	public static void main(String[] args) throws IOException {
+		String host = "http://transport.opendata.ch/";
+		String ressource = "v1/locations";
+		String query = "?query=Zürich";
+		URL url = new URL(host + ressource + query);
+		WebServiceClient wsc = new WebServiceClient(url);
+		
+		InputStream is = wsc.getInputStream();
+		JSONTokener jt = new JSONTokener(is);
+		JSONObject jo = (JSONObject)jt.nextValue();
+		JSONArray ja = (JSONArray) jo.get("stations");
+		JSONObject station = (JSONObject) ja.get(0);
+		System.out.println(station.get("coordinate"));
+	}
 
 }
