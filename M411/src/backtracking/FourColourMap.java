114806099c0b350a3a60db127c2179bbc4234774
capa <capa@cascianos-mbp-2.lan>
2019-01-08 05:06:32 +0100

    Improvements.

--- M411/src/backtracking/FourColourMap.java
+++ M411/src/backtracking/FourColourMap.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class FourColourMap {
 
-	HashMap<Integer,ArrayList> map = new HashMap<Integer,ArrayList>();
+	HashMap<Integer, ArrayList> map = new HashMap<Integer, ArrayList>();
 	boolean foundsolution = false;
-	
+
 	public FourColourMap() {
 		initMap();
 	}
-	
+
 	private void initMap() {
 		ArrayList<Integer> zero = new ArrayList<Integer>();
 		zero.add(1);
 		zero.add(3);
 		zero.add(4);
 		map.put(0, zero);
-		
+
 		ArrayList<Integer> first = new ArrayList<Integer>();
 		first.add(0);
 		first.add(2);
@@ -27,19 +27,19 @@ public class FourColourMap {
 		first.add(4);
 		first.add(5);
 		map.put(1, first);
-		
+
 		ArrayList<Integer> second = new ArrayList<Integer>();
 		second.add(1);
 		second.add(4);
 		second.add(5);
 		map.put(2, second);
-		
+
 		ArrayList<Integer> third = new ArrayList<Integer>();
 		third.add(0);
 		third.add(1);
 		third.add(4);
 		map.put(3, third);
-		
+
 		ArrayList<Integer> fourth = new ArrayList<Integer>();
 		fourth.add(0);
 		fourth.add(1);
@@ -47,74 +47,80 @@ public class FourColourMap {
 		fourth.add(3);
 		fourth.add(5);
 		map.put(4, fourth);
-		
+
 		ArrayList<Integer> fifth = new ArrayList<Integer>();
 		fifth.add(1);
 		fifth.add(2);
 		fifth.add(4);
 		map.put(5, fifth);
 	}
-	
+
 	public void solve() {
 		int[] a = new int[map.size()];
 		// initially the map is not coloured
-		for (int i=0; i<a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			a[i] = -1;
 		}
 		solve(a, 0);
 	}
-	
+
 	private void solve(int[] a, int i) {
-		if (a.length == i) { 
+		printMap(a);
+		if (a.length == i) {
 			System.out.println("Found solution");
-			printMap(a);
 			foundsolution = true;
-			return; 
-	    }
-		for (int j=0; j<4; j++) {
-			if (foundsolution) { break; }
-			a[i] = j; 
-			if (isConsitent(a,i)) {
-				solve(a,i+1);
-			} else {
-				System.out.println("Trying next possible solution...");
-			} 
+			return;
+		}
+		for (int j = 0; j < 4; j++) {
+			if (foundsolution) {
+				break;
+			}
+			a[i] = j;
+			if (isConsitent(a, i)) {
+				solve(a, i + 1);
+			}
+			System.out.println("Trying next possible solution...");
+
 		}
 		System.out.println("Backtracking...");
-		printMap(a);
-		
+
 	}
-	
 
 	private boolean isConsitent(int[] a, int i) {
+		System.out.println("Checking consistency until up [" + i + "]");
+		/**
+		 * Get the colours of the neighbours and check that no two neighbours have
+		 * the same colour.
+		 */
 		ArrayList<Integer> neighbours;
-		for (int j = 0; j<=i; j++) {
+		for (int j = 0; j <= i; j++) {
 			neighbours = map.get(j);
 			int neighbour = 0;
-			for (int k=0; k<neighbours.size(); k++) {				
+			for (int k = 0; k < neighbours.size(); k++) {
 				neighbour = neighbours.get(k);
-				if (j==neighbour) { continue; }
-				if (a[j] == a[neighbour]) { 
-					return false; 
+				if (a[j] == a[neighbour]) {
+					return false;
 				}
-			}			
+			}
 		}
 		return true;
 	}
 
 	private void printMap(int[] a) {
-		for (int i=0; i<a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i]);
-			if (i<a.length-1) { System.out.print(","); }
+			if (i < a.length - 1) {
+				System.out.print(",");
+			}
 		}
 		System.out.println();
-		
+
 	}
 
 	public static void main(String[] args) {
 		FourColourMap fourColourMap = new FourColourMap();
 		fourColourMap.solve();
-		
+
 	}
 
 }
