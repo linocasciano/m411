fc64ea18811886eb021221e3cb692abd88a28c32
Lino <Lino@Blackbox>
2019-01-11 05:09:25 +0100

    Improvements

--- M411/src/recusion/BinarySearch.java
+++ M411/src/recusion/BinarySearch.java
@@ -6,34 +6,46 @@ import helper.Util;
 
 public class BinarySearch {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// binary search needs a sorted array
 		// int[] a = Util.fillrand(100000003);
-		int[] a = { 1, 4, 6, 88, 123, 456, 1000 };
-		int needle = 100;
+		int[] a = { 1, 4, 6, 88, 123, 456, 900, 1000 };
+		int needle = 3;
 		// a[0] = needle;
 		Arrays.sort(a);
 		// Util.printArray(a);
-		System.out.println("Searching for " + needle);
-		binSearch(needle, a, 0, a.length - 1);
+		System.out.println("Searching for [" + needle + "]");
+		if (binSearch(needle, a, 0, a.length - 1)) {
+			System.out.println("Found!");
+		} else {
+			System.out.println("Not found!");
+		}
 
 	}
 
-	private static void binSearch(int needle, int[] a, int low, int high) {
+	/**
+	 * @param needle
+	 * @param a
+	 * @param low
+	 * @param high
+	 */
+	private static boolean binSearch(int needle, int[] a, int low, int high) {
 		System.out.println("low = " + low + ", high=" + high);
-		if (low < high) {
+		if (low <= high) {
 			int i = (low + high) / 2;
 			if (a[i] == needle) {
-				System.out.println("Found!");
-				return;
+				return true;
 			}
 			if (a[i] < needle) {
-				binSearch(needle, a, i + 1, high);
+				return binSearch(needle, a, i + 1, high);
 			} else {
-				binSearch(needle, a, low, i - 1);
+				return binSearch(needle, a, low, i - 1);
 			}
 		}
-
+		return false;
 	}
 
 }
