9660736cc81839676dba987e70f13f336bdd2653
Lino <Lino@Blackbox>
2019-01-13 15:51:09 +0100

    Implementation completed.

--- M411/src/recusion/CountLetters.java
+++ M411/src/recusion/CountLetters.java
@@ -7,30 +7,46 @@ public class CountLetters {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);	
 		
-		System.out.println( "Geben Sie das zu überprüfende Wort ein:" );
+		System.out.print( "Geben Sie das zu überprüfende Wort ein:" );
 
 		String word = scan.nextLine();
 		
-		System.out.println( "Geben Sie den Buchstaben an, nach dem gesucht werden soll:" );
+		System.out.println();
+		
+		System.out.print( "Geben Sie den Buchstaben an, nach dem gesucht werden soll:" );
+		
+		System.out.println();
 
 		String line = scan.nextLine();
 		
 		System.out.println("Searching for [" + line.charAt(0) + "] in [" + word + "]");
 		
-		Integer counter = 0;
-		charCount(line.charAt(0), word, 0, counter);
+		int counter = charCount(line.charAt(0), word, 0);
 		
 		System.out.println(line.charAt(0) + " found " + counter + " times.");
+		
+		 
 		scan.close();
 
 	}
 
-	private static void charCount(char charValue, String word, int pos, Integer counter) {
-		if (pos >= word.length()) { return; }
+	private static int charCount(char charValue, String word, int pos) {
+		int counter = 0;
+		if (pos >= word.length()) { return counter ; }
 		if (charValue == word.charAt(pos)) {
-			counter = counter + 1;
+			counter++;
 		}
-		charCount(charValue, word, pos+1, counter);		
+		counter += charCount(charValue, word, pos+1);
+		return counter;
+	}
+	
+	private static int charCount(char charValue, String word, int pos, int counter) {
+		if (pos >= word.length()) { return counter ; }
+		if (charValue == word.charAt(pos)) {
+			counter++;
+		}
+		counter = charCount(charValue, word, pos+1, counter);
+		return counter;
 	}
 
 }
